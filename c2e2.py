1866c55a5d8cbff4b578b467ec8d4aac15ccb946
Valeria Arias <v.arias@correo.unimet.edu.ve>
2021-05-04 22:27:05 -0400

    listo c2e2

---
+++ c2e2.py
@@ -0,0 +1,7 @@
+#Imprimir mensajes con variables
+
+username = 'Ana'
+timestamp = '07:00'
+url = "https://petshop.com/pets/reptiles/pythons"
+
+print(f'{username} ingreso al sitio {url} a las {timestamp}A.M.')
